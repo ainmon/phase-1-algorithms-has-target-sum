cd2e8f94bee49e9f3cca824261a4b3960e68b240
Adam <inmon.adam@yahoo.com>
2021-10-30 14:29:00 -0500

    commit tree f9d84869bfa9344cc669492191436952e782bcd3

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    let difference = target - array[i];
+    for(let j = i + 1; j < array.length; j++){
+      console.log(array[j], difference);
+      if(array[j] === difference){
+        return true;
+      }
+      else if (array.length === 1){
+        console.log(array, target);
+        return false;
+      }
+    }
+  }
+  return false;
 }
 
+
 /* 
-  Write the Big O time complexity of your function here
+  O(n*2)
 */
 
 /* 
-  Add your pseudocode here
+  We need to loop through the array initially and then loop once again so we can compare the numbers in our array
+  we then need to add them and compare them to our target
+  if it equals to our target, it will return true
+  otherwise, if there is only one number in our array or it doesn't equal to our target, it will return false
+  *we need to be careful with our returns (the false might override arrays that should be returning true)*
 */
 
 /*
